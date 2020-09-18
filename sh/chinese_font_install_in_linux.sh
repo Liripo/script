fddfe6d0e03d88980ee453ebff60c7f21364b2f9
Liripo <54770415+Liripo@users.noreply.github.com>
2020-09-18 16:53:38 +0800

    未来荧黑中文字体安装

---
+++ sh/chinese_font_install_in_linux.sh
@@ -0,0 +1,19 @@
+if [ ! -d "~/.fonts"];then
+  mkdir ~/.fonts
+  echo "创建文件夹~/.fonts"
+fi
+#安装未来荧黑简体，网址;[https://github.com/welai/glow-sans]
+glowsan=https://github.com/welai/glow-sans/releases/download/v0.9/GlowSansSC-Normal-v0.9.zip
+#pwd
+work_path=`pwd`
+cd ~/.fonts
+wget $glowsan
+
+unzip ${glowsan##*/}
+#生成字体缓存
+fc-cache
+
+#查看中文字体
+fc-list :lang=zh
+
+cd $work_path
